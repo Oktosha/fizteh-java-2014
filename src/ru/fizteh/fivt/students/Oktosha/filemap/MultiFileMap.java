6eae1fb3d1efa5d8fdae5adb82b2b2c7636781e4
Daria Kolodzey <kolodaria@google.com>
2015-03-03 19:54:30 +0300

    wrote constructor for MultiFileMap

--- src/ru/fizteh/fivt/students/Oktosha/filemap/MultiFileMap.java
+++ src/ru/fizteh/fivt/students/Oktosha/filemap/MultiFileMap.java
@@ -16,8 +16,17 @@ public class MultiFileMap {
     public MultiFileMap(Path path) throws IOException{
         this.path = path;
         this.fileMaps = new FileMap[FileMapPosition.DIR_PER_TABLE][FileMapPosition.FILES_PER_DIR];
+        for (int directoryId = 0; directoryId < FileMapPosition.DIR_PER_TABLE; ++directoryId) {
+            Path directoryPath = path.resolve(FileMapPosition.relPathToDirectory(directoryId));
+            if (directoryPath.toFile().exists() && !directoryPath.toFile().isDirectory()) {
+                throw new IOException("bd dir is not a dir: " + directoryPath.toString());
+            }
+            for (int fileId = 0; fileId < FileMapPosition.FILES_PER_DIR; ++fileId) {
+                Path fileMapPath = path.resolve(FileMapPosition.relPathToFileMap(directoryId, fileId));
+                fileMaps[directoryId][fileId] = new FileMap(fileMapPath);
+            }
+        }
     }
-
 }
 
 class FileMapPosition {
@@ -33,6 +42,15 @@ class FileMapPosition {
         this(key.hashCode() % DIR_PER_TABLE, key.hashCode() / DIR_PER_TABLE % FILES_PER_DIR);
     }
     Path relPathToFileMap() {
+        return relPathToFileMap(directoryId, fileId);
+    }
+    Path relPathToDirectory() {
+        return relPathToDirectory(directoryId);
+    }
+    static Path relPathToDirectory(int directoryId) {
+        return Paths.get(String.format("%d.dir", directoryId));
+    }
+    static Path relPathToFileMap(int directoryId, int fileId) {
         return Paths.get(String.format("%d.dir/%d.dat", directoryId, fileId));
     }
 }
