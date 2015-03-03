f3c6e39b126b4e6f6a61ff7250a439c3ed778050
Daria Kolodzey <kolodaria@google.com>
2015-03-03 23:11:13 +0300

    wrote test for MultiFileMap, all passed

--- src/ru/fizteh/fivt/students/Oktosha/filemap/MultiFileMapTest.java
+++ src/ru/fizteh/fivt/students/Oktosha/filemap/MultiFileMapTest.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import static org.junit.Assert.*;
 
@@ -23,37 +26,77 @@ public class MultiFileMapTest {
 
     @Test
     public void testSave() throws Exception {
-
+        multiFileMap.save();
+        MultiFileMap loadedMultiFileMap = new MultiFileMapImpl(path);
+        List<String> listOfKeys = multiFileMap.list();
+        List<String> loadedListOfKeys = loadedMultiFileMap.list();
+        assertEquals(new HashSet<String>(listOfKeys), new HashSet<String>(loadedListOfKeys));
+        for (String key : listOfKeys) {
+            assertEquals(multiFileMap.get(key), loadedMultiFileMap.get(key));
+        }
     }
 
     @Test
     public void testClear() throws Exception {
-
+        multiFileMap.clear();
+        assertEquals(0, multiFileMap.size());
+        assertEquals(0, multiFileMap.list().size());
+        assertNull(multiFileMap.get("key"));
+        assertNull(multiFileMap.get("ключ"));
+        assertNull(multiFileMap.get("unknown key"));
     }
 
     @Test
     public void testPut() throws Exception {
-
+        assertEquals("value", multiFileMap.put("key", "value"));
+        assertEquals("value", multiFileMap.get("key"));
+        assertEquals("value", multiFileMap.put("key", "new value"));
+        assertEquals("new value", multiFileMap.get("key"));
+        assertNull(multiFileMap.put("new key", "some value"));
+        assertEquals("some value", multiFileMap.put("new key", "значение"));
     }
 
     @Test
     public void testGet() throws Exception {
-
+        assertEquals("value", multiFileMap.get("key"));
+        assertEquals("значение", multiFileMap.get("ключ"));
+        assertNull(multiFileMap.get("unknown key"));
     }
 
     @Test
     public void testRemove() throws Exception {
-
+        assertNull(multiFileMap.remove("незивестный ключ"));
+        assertEquals("value", multiFileMap.remove("key"));
+        assertNull(multiFileMap.remove("key"));
+        assertEquals("значение", multiFileMap.remove("ключ"));
+        assertNull(multiFileMap.remove("ключ"));
+        assertNull(multiFileMap.remove("unknown key"));
     }
 
     @Test
     public void testSize() throws Exception {
-
+        assertEquals(2, multiFileMap.size());
+        multiFileMap.put("new key", "new value");
+        assertEquals(3, multiFileMap.size());
+        multiFileMap.put("key", "value");
+        assertEquals(3, multiFileMap.size());
+        multiFileMap.put("key", "new value");
+        assertEquals(3, multiFileMap.size());
+        multiFileMap.get("key");
+        assertEquals(3, multiFileMap.size());
+        multiFileMap.remove("key");
+        assertEquals(2, multiFileMap.size());
+        multiFileMap.clear();
+        assertEquals(0, multiFileMap.size());
     }
 
     @Test
     public void testList() throws Exception {
-
+        Set<String> keysInMap = new HashSet<>(multiFileMap.list());
+        Set<String> expectedKeys = new HashSet<>(2);
+        expectedKeys.add("key");
+        expectedKeys.add("ключ");
+        assertEquals(expectedKeys, keysInMap);
     }
 
     @Test(expected = IllegalArgumentException.class)
