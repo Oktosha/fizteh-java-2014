5f4510a90c503128750af90ad06f565f44f61c20
Daria Kolodzey <kolodaria@google.com>
2015-03-03 19:32:31 +0300

    created class for holding filemap coordinates inside multifilemap

--- src/ru/fizteh/fivt/students/Oktosha/filemap/MultiFileMap.java
+++ src/ru/fizteh/fivt/students/Oktosha/filemap/MultiFileMap.java
@@ -1,8 +1,8 @@
 package ru.fizteh.fivt.students.Oktosha.filemap;
 
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * Created by DKolodzey on 02.03.15.
@@ -15,6 +15,24 @@ public class MultiFileMap {
 
     public MultiFileMap(Path path) throws IOException{
         this.path = path;
-        this.fileMaps = new FileMap[16][16];
+        this.fileMaps = new FileMap[FileMapPosition.DIR_PER_TABLE][FileMapPosition.FILES_PER_DIR];
+    }
+
+}
+
+class FileMapPosition {
+    static final int DIR_PER_TABLE = 16;
+    static final int FILES_PER_DIR = 16;
+    private int directoryId;
+    private int fileId;
+    FileMapPosition(int directoryId, int fileId) {
+        this.directoryId = directoryId;
+        this.fileId = fileId;
+    }
+    FileMapPosition(String key) {
+        this(key.hashCode() % DIR_PER_TABLE, key.hashCode() / DIR_PER_TABLE % FILES_PER_DIR);
+    }
+    Path relPathToFileMap() {
+        return Paths.get(String.format("%d.dir/%d.dat", directoryId, fileId));
     }
 }
