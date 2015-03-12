35b1a4f20d5cc5c40adc12f11b2641d79ab4a4f8
Daria Kolodzey <kolodaria@google.com>
2015-03-12 14:14:18 +0300

    wrote TableProviderFactory

---
+++ src/ru/fizteh/fivt/students/Oktosha/filemap/TableProviderFactoryImpl.java
@@ -0,0 +1,17 @@
+package ru.fizteh.fivt.students.Oktosha.filemap;
+
+import ru.fizteh.fivt.storage.structured.TableProvider;
+import ru.fizteh.fivt.storage.structured.TableProviderFactory;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+/**
+ * Created by DKolodzey on 12.03.15.
+ */
+public class TableProviderFactoryImpl implements TableProviderFactory {
+    @Override
+    public TableProvider create(String path) throws IOException {
+        return new TableProviderImpl(Paths.get(path));
+    }
+}
