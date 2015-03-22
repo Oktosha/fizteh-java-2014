3e9697358f23315a27eab669ba8896a8c3a03915
Daria Kolodzey <kolodaria@google.com>
2015-03-22 23:20:05 +0300

    wrote rollback commit and size

---
+++ src/ru/fizteh/fivt/students/Oktosha/servlet/servlets/RollbackServlet.java
@@ -0,0 +1,28 @@
+package ru.fizteh.fivt.students.Oktosha.servlet.servlets;
+
+import ru.fizteh.fivt.students.Oktosha.database.storeable.DroppableStructuredTable;
+import ru.fizteh.fivt.students.Oktosha.servlet.ServletContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by DKolodzey on 23.03.15.
+ */
+public class RollbackServlet extends AbstractServlet {
+    protected RollbackServlet(ServletContext context) {
+        super(context);
+    }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        DroppableStructuredTable switchedTable = switchToTransaction(req, resp);
+        try {
+            resp.getWriter().write(switchedTable.rollback());
+            resp.setStatus(200);
+        } catch (Throwable e) {
+            resp.sendError(500, e.getMessage());
+        }
+    }
+}
