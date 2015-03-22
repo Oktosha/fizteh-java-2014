07ff2cd38863f54ac0893bc3ea42ac747ffa637e
Daria Kolodzey <kolodaria@google.com>
2015-03-23 00:56:23 +0300

    wrote toString for storeable

---
+++ src/ru/fizteh/fivt/students/Oktosha/database/storeable/StoreableImplTest.java
@@ -0,0 +1,13 @@
+package ru.fizteh.fivt.students.Oktosha.database.storeable;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StoreableImplTest {
+
+    @Test
+    public void testToString() throws Exception {
+
+    }
+}
