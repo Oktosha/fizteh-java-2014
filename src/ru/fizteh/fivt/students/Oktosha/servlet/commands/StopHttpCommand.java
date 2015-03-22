9803201198e033951fa69cf8b7ec062c966771c5
Daria Kolodzey <kolodaria@google.com>
2015-03-22 22:25:25 +0300

    stophtpp

--- src/ru/fizteh/fivt/students/Oktosha/servlet/commands/StopHttpCommand.java
+++ src/ru/fizteh/fivt/students/Oktosha/servlet/commands/StopHttpCommand.java
@@ -4,33 +4,30 @@ import ru.fizteh.fivt.students.Oktosha.commander.AbstractCommand;
 import ru.fizteh.fivt.students.Oktosha.commander.CommandExecutionException;
 import ru.fizteh.fivt.students.Oktosha.commander.Context;
 import ru.fizteh.fivt.students.Oktosha.servlet.HTTPServer;
-import ru.fizteh.fivt.students.Oktosha.servlet.ServletContextImpl;
 
-import java.net.InetSocketAddress;
 import java.util.List;
 
 /**
  * Created by DKolodzey on 22.03.15.
  */
-public class StartHttpCommand extends AbstractCommand {
+public class StopHttpCommand extends AbstractCommand {
     @Override
     public String getName() {
-        return "starthttp";
+        return "stophttp";
     }
 
     @Override
     public void exec(Context context, List<String> arguments) throws CommandExecutionException {
-        checkArguments(arguments, 2);
+        checkArguments(arguments, 1);
         HTTPServer server = context.getServer();
-        if (server.isRunning()) {
-            throw new CommandExecutionException("not started: already started");
+        if (!server.isRunning()) {
+            throw new CommandExecutionException("not started");
         }
         try {
-            server.start(new InetSocketAddress(Integer.parseInt(arguments.get(1))),
-                    new ServletContextImpl(context.getDiffManager(), context.getTableProvider()));
-            println(context, "started at port" + arguments.get(1));
+            server.stop();
         } catch (Exception e) {
             throw new CommandExecutionException(e);
         }
+        println(context, "stopped");
     }
 }
